12f4ea59ebed94e4a011c06d72f8efb4bea8d79c
Bilder-Rikkyo <rikkyo.umigomi@gmail.com>
2024-11-02 21:41:54 +0900

    Update app.js

--- location/app.js
+++ location/app.js
@@ -14,7 +14,7 @@ const createPlaces = ({ latitude, longitude }) => [
   const models = [
     {
       url: '#animated-asset',
-      scale: ['1', '1', '1'],
+      scale: ['0.1', '0.1', '0.1'],
     },
   ];
   
